258cff25b4462978c4a40138431b51fdf8a8116b
strangestar <strangestar@users.noreply.github.com>
2016-02-28 20:21:17 -0500

    shell script for ruby

---
+++ ruby.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+
+\curl -sSL https://get.rvm.io | bash -s stable --ruby
+ruby -v
+
+#source /home/vagrant/.rvm/scripts/rvm
+#rvm reinstall ruby-2.2.1
+#ruby -v
+
+#gem install --no-ri --no-rdoc rails
+#rails -v
+
+#source /home/vagrant/.rvm/scripts/rvm
+#rvm reinstall ruby-2.2.1
+
