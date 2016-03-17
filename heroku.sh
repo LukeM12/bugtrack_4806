a5678df26162f7a7e536497dda1e1287ae643601
strangestar <strangestar@users.noreply.github.com>
2016-03-17 14:09:00 -0400

    added nodejs
    still need to add user vagrant

--- heroku.sh
+++ heroku.sh
@@ -2,5 +2,10 @@
 
 sudo apt-get install libpq-dev -y
 sudo apt-get install postgresql -y 
+#need to add user for postgres 
+#sudo -u postgres psql
+#create user vagrant with superuser
+#\q
+sudo apt-get install nodejs -y
 
 wget -O- https://toolbelt.heroku.com/install-ubuntu.sh | sh
