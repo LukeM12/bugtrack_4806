7c9d6659238bc60f9cbe41d75f40c79a64670979
strangestar <strangestar@users.noreply.github.com>
2016-03-17 10:25:46 -0400

    updated clone uri

--- repo.sh
+++ repo.sh
@@ -2,4 +2,4 @@
 echo "Installing git"
 sudo apt-get install git -y > /dev/null
 cd /vagrant
-git clone https://github.com/strangestar/BugHuntTest.git
+git clone https://github.com/strangestar/BugHunt4806.git
