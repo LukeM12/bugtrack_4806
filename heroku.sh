a105d980bc5f3b7ee762153534f12a5a3b6d90af
strangestar <s.macdonald@null.net>
2016-03-19 16:16:04 -0400

    now creates psql user

--- heroku.sh
+++ heroku.sh
@@ -2,10 +2,7 @@
 
 sudo apt-get install libpq-dev -y
 sudo apt-get install postgresql -y 
-#need to add user for postgres 
-#sudo -u postgres psql
-#create user vagrant with superuser
-#\q
+sudo -u postgres bash -c "psql -c \"CREATE USER vagrant WITH superuser;\""
 sudo apt-get install nodejs -y
 
 wget -O- https://toolbelt.heroku.com/install-ubuntu.sh | sh
